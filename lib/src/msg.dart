34e3451aa0cc19993ab584e1533b832703c78094
Frank Moreno <frankmoreno1993@gmail.com>
2018-04-30 14:42:21 -0500

    msg class: without warnings

--- lib/src/msg.dart
+++ lib/src/msg.dart
@@ -1,40 +1,8 @@
 import 'dart:typed_data';
 import 'package:nuid/nuid.dart';
+import 'package:nats/src/const.dart';
 
-
-final MSG_RE  = new RegExp('\AMSG\s+([^\s]+)\s+([^\s]+)\s+(([^\s]+)[^\S\r\n]+)?(\d+)\r\n');
-final OK_RE   = new RegExp('\A\+OK\s*\r\n');
-final ERR_RE  = new RegExp('\A-ERR\s+(\'.+\')?\r\n');
-final PING_RE = new RegExp('\APING\s*\r\n');
-final PONG_RE = new RegExp('\APONG\s*\r\n');
-final INFO_RE = new RegExp('\AINFO\s+([^\r\n]+)\r\n');
-
-const INFO_OP = 'INFO';
-const CONNECT_OP = 'CONNECT';
-const PUB_OP = 'PUB';
-const MSG_OP = 'MSG';
-const SUB_OP = 'SUB';
-const UNSUB_OP = 'UNSUB';
-const PING_OP = 'PING';
-const PONG_OP = 'PONG';
-const OK_OP = '+OK';
-const ERR_OP = '-ERR';
-const MSG_END = '\n';
-const _CRLF_ = '\r\n';
-const _SPC_ = ' ';
-const EMPTY = '';
-
-const OK = OK_OP + _CRLF_;
-const PING = PING_OP + _CRLF_;
-const PONG = PONG_OP + _CRLF_;
-const CRLF_SIZE = _CRLF_.length;
-const OK_SIZE = OK.length;
-const PING_SIZE = PING.length;
-const PONG_SIZE = PONG.length;
-const MSG_OP_SIZE = MSG_OP.length;
-const ERR_OP_SIZE = ERR_OP.length;
-
-String createInbox() => "_INBOX."+nuid.next();
+String createInbox() => "_INBOX." + nuid.next();
 
 class Msg {
   String subject;
@@ -55,7 +23,7 @@ class Msg {
       subjectAndReplyTo = '${subject} ${replyTo}';
     }
 
-    return "PUB ${subjectAndReplyTo} ${replyTo} ${n}\r\n${payload}\r\n";
+    return "${PUB_OP} ${subjectAndReplyTo} ${replyTo} ${n}\r\n${payload}\r\n";
   }
 
   static String sub(String subject, int sid, [String queueGroup]){
@@ -63,7 +31,7 @@ class Msg {
     if(queueGroup != null && queueGroup.isNotEmpty){
       subjectAndQueue = '${subject} ${queueGroup}';
     }
-    return "SUB ${subjectAndQueue} ${sid}\r\n";
+    return "${SUB_OP} ${subjectAndQueue} ${sid}\r\n";
   }
 
   static String unsub(int sid, [int maxMsg]){
@@ -71,6 +39,6 @@ class Msg {
     if(maxMsg != null){
       sidAndMax = '${sid} ${maxMsg}';
     }
-    return "UNSUB ${sidAndMax}\r\n";
+    return "${UNSUB_OP} ${sidAndMax}\r\n";
   }
 }
